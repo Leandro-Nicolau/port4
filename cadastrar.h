6b0cccab5acf6d94ddd180d3ae9b527e91a1679f
Leandro-Nicolau <leandronicolau82@gmail.com>
2026-06-08 09:34:18 -0300

    Update cadastrar.h

--- cadastrar.h
+++ cadastrar.h
@@ -1,6 +1,22 @@
 #ifndef CADASTRAR_H
 #define CADASTRAR_H
 
+void cadastrarAluno(int quantidade);
+void listarAlunos(int quantidade);
+
+void cadastrarProfessor(int quantidade);
+void listarProfessores(int quantidade);
+
+void cadastrarTurma(int quantidade);
+void listarTurmas(int quantidade);
+
+void cadastrarDisciplina(int quantidade);
+void listarDisciplinas(int quantidade);
+
+void cadastrarCurso(int quantidade);
+void listarCursos(int quantidade);
+
 void cadastrarFuncionario(int quantidade);
+void listarFuncionarios(int quantidade);
 
 #endif
